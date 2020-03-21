71de0593c4a2129ffb5ee95c391cec28644dd8d7
Iris <18377007@buaa.edu.cn>
2020-03-21 14:34:30 +0800

    node

---
+++ node.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 20 15:26:52 2020
+
+@author: Iris
+"""
+
+class Node:
+    def __init__(self, initdata):
+        self.data = initdata
+        self.next = None
+    
+    def getData(self):
+        return self.data
+    
+    def getNext(self):
+        return self.next
+    
+    def setData(self, newdata):
+        self.data = newdata
+        
+    def setNext(self, newnext):
+        self.next = newnext
